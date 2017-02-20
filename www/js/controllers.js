2187a8a22387c21b029d619145c88b9bda64d4ca
Adrien Juhem <adrien.juhem@gmail.com>
2017-02-20 23:42:12 +0100

    get api

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,7 +2,8 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
-.controller('ChatsCtrl', function($scope, Chats) {
+
+.controller('ChatsCtrl', ['$scope','$http', function($scope, $http) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -10,14 +11,28 @@ angular.module('starter.controllers', [])
   //
   //$scope.$on('$ionicView.enter', function(e) {
   //});
+  $http.get('http://carbillet.net/api-digitalGrenoble/users/')
+  .then(function successCallback(response) {
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
+    $scope.users = response.data.users;
+    console.log(response);  
+
+
+  }, function errorCallback(response) {
+    console.log("error");
+  });
+
+  // $scope.chats = Chats.all();
+  // $scope.remove = function(chat) {
+  //   Chats.remove(chat);
+  // };
+}])
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
 })
 
-.controller('AccountCtrl', function($scope) {
+.controller('SettingCtrl', function($scope) {
   $scope.settings = {
     enableFriends: true
   };
--- www/js/services.js
+++ www/js/services.js
@@ -1,40 +1,50 @@
 angular.module('starter.services', [])
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
+// .factory('Chats', function() {
+//   // Might use a resource here that returns a JSON array
 
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Jérôme Lombard',
-    lastText: 'Je vais mouillir !',
-    face: 'img/64014_10151222269295073_1813267606_n.jpg'
-  }, {
-    id: 1,
-    name: 'Adrien Juhem',
-    lastText: 'Salut ! C\'est moi les loulous',
-    face: 'img/1912230_10203764604296067_6311840730912212111_n.jpg'
-  }, {
-    id: 2,
-    name: 'Joel Ponson',
-    lastText: 'Je vais te taper là !',
-    face: 'img/11069866_10205374576696270_5295172757254849060_n.jpg'
-  }];
+//   // Some fake testing data
+//   var chats = [{
+//     id: 0,
+//     name: 'Ben Sparrow',
+//     lastText: 'You on your way?',
+//     face: 'img/ben.png'
+//   }, {
+//     id: 1,
+//     name: 'Max Lynx',
+//     lastText: 'Hey, it\'s me',
+//     face: 'img/max.png'
+//   }, {
+//     id: 2,
+//     name: 'Adam Bradleyson',
+//     lastText: 'I should buy a boat',
+//     face: 'img/adam.jpg'
+//   }, {
+//     id: 3,
+//     name: 'Perry Governor',
+//     lastText: 'Look at my mukluks!',
+//     face: 'img/perry.png'
+//   }, {
+//     id: 4,
+//     name: 'Mike Harrington',
+//     lastText: 'This is wicked good ice cream.',
+//     face: 'img/mike.png'
+//   }];
 
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
-        }
-      }
-      return null;
-    }
-  };
-});
+//   return {
+//     all: function() {
+//       return chats;
+//     },
+//     remove: function(chat) {
+//       chats.splice(chats.indexOf(chat), 1);
+//     },
+//     get: function(chatId) {
+//       for (var i = 0; i < chats.length; i++) {
+//         if (chats[i].id === parseInt(chatId)) {
+//           return chats[i];
+//         }
+//       }
+//       return null;
+//     }
+//   };
+// });
