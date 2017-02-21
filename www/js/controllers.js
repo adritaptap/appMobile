27a05ced45e9a865b45649c3dba6b4e67dcc0d52
joelponson <ponson.joel@gmail.com>
2017-02-21 09:11:40 +0100

    first commit joel branch

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -17,6 +17,21 @@ angular.module('starter.controllers', [])
   };
 })
 
+.controller('FriendsCtrl', function($scope, Friends) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.friends = Friends.all();
+  $scope.remove = function(friend) {
+    Friends.remove(friend);
+  };
+})
+
 .controller('AccountCtrl', function($scope) {
   $scope.settings = {
     enableFriends: true
